3129785447363efb9bbbfd6edaf4c43a088ef46d
jgineral <33348154+jgineral@users.noreply.github.com>
2023-03-13 14:20:31 +0100

    Create Ingrediente POJO
    Add ingredient to the recipe.
    First implementation.

--- app/controllers/RecetasController.java
+++ app/controllers/RecetasController.java
@@ -4,7 +4,10 @@ import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.mvc.Results;
+import views.IngredienteView;
 import views.RecetaView;
+
+import java.util.List;
 
 public class RecetasController extends Controller {
 
@@ -12,10 +15,18 @@ public class RecetasController extends Controller {
         Result result;
         String name = request.queryString("name").orElse("");
         String stars = request.queryString("stars").orElse("");
+        String desc = request.queryString("description").orElse("");
+        String ingredientName = request.queryString("ingredientName").orElse("");
 
         RecetaView recetaView = new RecetaView();
         recetaView.setName(name);
         recetaView.setStars(Integer.valueOf(stars));
+        recetaView.setDescription(desc);
+
+        IngredienteView ingredient = new IngredienteView();
+        ingredient.setName(ingredientName);
+
+        recetaView.getIngredients().add(ingredient);
 
 
         result = Results.created(recetaView.toJson())
--- app/views/RecetaView.java
+++ app/views/RecetaView.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import play.libs.Json;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class RecetaView {
     private String name;
-
     private Integer stars;
     private String description;
+    private List<IngredienteView> ingredients = new LinkedList<>();
 
     public String getName() {
         return name;
@@ -24,6 +27,22 @@ public class RecetaView {
 
     public void setStars(Integer stars) {
         this.stars = stars;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<IngredienteView> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<IngredienteView> ingredients) {
+        this.ingredients = ingredients;
     }
 
     public JsonNode toJson() {
